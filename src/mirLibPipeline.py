2d313a1c4aa7bc52d9428648615c3a70d75e8d42
Julie Wu <wu.chaojung@gmail.com>
2017-04-10 10:56:14 -0400

    merge with master Amine modification on refactor filtration class etc

--- src/mirLibPipeline.py
+++ src/mirLibPipeline.py
@@ -20,6 +20,11 @@ personal note - julie's launching command linein her computer:
 spark-submit mirLibPipeline.py a_thaliana /home/cloudera/workspace/miRNA_predictor/sudoData/a_th_3.txt 2>/dev/null
 
 spark-submit mirLibPipeline.py a_thaliana /home/cloudera/workspace/miRNA_predictor/sudoData/a_th_10.txt 2>/dev/null
+
+$ time spark-submit mirLibPipeline.py a_thaliana /home/cloudera/workspace/miRNA_predictor/sudoData/110.txt 1>170406_1result_pipeline.txt 2>/dev/null
+around 10 mins (6m, 12m, 9.5min)
+
+$ time spark-submit mirLibPipeline.py a_thaliana /home/cloudera/workspace/miRNA_predictor/sudoData/1102.txt 1>/home/cloudera/workspace/miRNA_predictor/logOutput/170407_result_pipeline_1.txt 2>/dev/null
 '''
 
 import sys
