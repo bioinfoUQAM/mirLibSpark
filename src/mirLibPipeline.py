8651c78d1d52d3e041a49218f5c203ca96d1d172
Julie Wu <wu.chaojung@gmail.com>
2017-04-21 11:55:58 -0700

    prototype v1.01

--- src/mirLibPipeline.py
+++ src/mirLibPipeline.py
@@ -14,41 +14,31 @@ Le programme implemente le pipeline d'analyse des sRAN et prediction des miRNAs.
   - 6 validaiton/filtrage avec l'expression
   
 La version actuelle accepte un seul argument qui est le fichier contenant les sequences reads a traiter.
-
-personal note - julie's launching command line in her computer:
-
-spark-submit mirLibPipeline.py a_thaliana /home/cloudera/workspace/miRNA_predictor/sudoData/a_th_3.txt 2>/dev/null
-
-spark-submit mirLibPipeline.py a_thaliana /home/cloudera/workspace/miRNA_predictor/sudoData/a_th_10.txt 2>/dev/null
-
-$ time spark-submit mirLibPipeline.py a_thaliana /home/cloudera/workspace/miRNA_predictor/sudoData/110.txt 1>170406_1result_pipeline.txt 2>/dev/null
-around 10 mins (6m, 12m, 9.5min)
-
-$ time spark-submit mirLibPipeline.py a_thaliana /home/cloudera/workspace/miRNA_predictor/sudoData/1102.txt 1>/home/cloudera/workspace/miRNA_predictor/logOutput/170407_result_pipeline_1.txt 2>/dev/null
-
-##########################
-spark-submit mirLibPipeline.py ../paramfile_julie.txt /home/cloudera/workspace/miRNA_predictor/sudoData/a_th_3.txt 2>/dev/null
-######
-batch
-time spark-submit mirLibPipeline.py ../paramfile_julie_batch.txt /home/cloudera/workspace/miRNA_predictor/sudoData2/ 2>/dev/null
 '''
 
-import os.path
 import sys
+import os.path
+from os import listdir
 #
 import utils as ut
 import mirLibRules as mru
-#
-from os import listdir
-from os.path import isfile, join
 
 
-def SparkTask(paramDict, infile):
+if __name__ == '__main__' :
+
+  if not len(sys.argv) == 3:
+    sys.stderr.write('Two arguments required\nUsage: spark-submit mirLibPipeline.py <path to paramfile> <path to your input> 2>/dev/null\n')
+    sys.exit()
+
+  paramfile = sys.argv[1]
+  mypath = sys.argv[2] 
+
+  paramDict = ut.readparam (paramfile)
 
   # Parameters and cutoffs
   my_sep = paramDict['my_sep']                # Separator
   rep_tmp = paramDict['rep_tmp']              # tmp file folder
-  rep_data = paramDict['rep_data'] 
+  rep_output = paramDict['rep_output'] 
   # spark parameter
   master = paramDict['master']                #"local" 
   appname = paramDict['appname']              #"mirLibHadoop"
@@ -68,21 +58,11 @@ def SparkTask(paramDict, infile):
   # mircheck parameter
   mcheck_param = paramDict['mcheck_param']    #'def'     # def : default parameters / mey : meyers parameters
 
-  inBasename = os.path.splitext(os.path.basename(infile))[0]
-  inKvfile = rep_tmp + inBasename + '.kv.txt'
-  hdfsFile = inBasename + '.hkv.txt'
-
   # Spark context
   sc = ut.pyspark_configuration(master, appname, memory) # yarn-client
   sc.addPyFile('utils.py')
   sc.addPyFile('mirLibRules.py')
-  
-  # Convert the input file to a Key value file
-  ut.convert_seq_freq_file_to_KeyValue(infile, inKvfile, my_sep)
-  
-  # Save a local file to HDFS system
-  ut.convertTOhadoop(inKvfile, hdfsFile)
-  
+
   # Object for rule functions
 
   dmask_obj = mru.prog_dustmasker()
@@ -91,83 +71,75 @@ def SparkTask(paramDict, infile):
   rnafold_obj = mru.prog_RNAfold()
   mircheck_obj = mru.prog_mirCheck(mcheck_param)
   profile_obj = mru.prog_dominant_profile()
-  
-  # Convert the text file to RDD object
-  distFile = sc.textFile(hdfsFile)
-  input_rdd = distFile.map(lambda line: mru.rearrange_rule(line, my_sep))
-  
-  # Filtering low frequency
-  rm_low_rdd = input_rdd.filter(lambda elem: int(elem[1][1]) > limit_freq)
 
-  # Filtering short length 
-  rm_short_rdd = rm_low_rdd.filter(lambda elem: len(str(elem[1][0])) > limit_len)
+  # fetch library files in mypath
+  infiles = [f for f in listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
 
-  # Filtering with DustMasker
-  dmask_rdd = rm_short_rdd.filter(dmask_obj.dmask_filter_rule)
-  
-  # Mapping with Bowtie
-  bowtie_rdd = dmask_rdd.map(bowtie_obj.Bowtie_map_rule).persist()
+  for infile in infiles:
+    infile = mypath+infile
+    inBasename = os.path.splitext(os.path.basename(infile))[0]
+    inKvfile = rep_tmp + inBasename + '.kv.txt'
+    hdfsFile = inBasename + '.hkv.txt'
 
-  # Filtering high nbLocations and zero location
-  nbLoc_rdd = bowtie_rdd.filter(lambda elem: elem[1][2] > 0 and elem[1][2] < limit_nbLoc)
+    # Convert the input file to a Key value file
+    ut.convert_seq_freq_file_to_KeyValue(infile, inKvfile, my_sep)
   
-  # Extraction of the pri-miRNA
-  primir_rdd = nbLoc_rdd.flatMap(prec_obj.extract_prim_rule)
+    # Save a local file to HDFS system
+    ut.convertTOhadoop(inKvfile, hdfsFile)
+  
+    # Convert the text file to RDD object
+    distFile = sc.textFile(hdfsFile)
+    input_rdd = distFile.map(lambda line: mru.rearrange_rule(line, my_sep))
+  
+    # Filtering low frequency
+    rm_low_rdd = input_rdd.filter(lambda elem: int(elem[1][1]) > limit_freq)
+
+    # Filtering short length 
+    rm_short_rdd = rm_low_rdd.filter(lambda elem: len(str(elem[1][0])) > limit_len)
+
+    # Filtering with DustMasker
+    dmask_rdd = rm_short_rdd.filter(dmask_obj.dmask_filter_rule)
+  
+    # Mapping with Bowtie
+    bowtie_rdd = dmask_rdd.map(bowtie_obj.Bowtie_map_rule).persist()
+
+    # Filtering high nbLocations and zero location
+    nbLoc_rdd = bowtie_rdd.filter(lambda elem: elem[1][2] > 0 and elem[1][2] < limit_nbLoc)
+  
+    # Extraction of the pri-miRNA
+    primir_rdd = nbLoc_rdd.flatMap(prec_obj.extract_prim_rule)
    
-  # pri-miRNA folding
-  pri_fold_rdd = primir_rdd.map(lambda elem: rnafold_obj.RNAfold_map_rule(elem, 4))
+    # pri-miRNA folding
+    pri_fold_rdd = primir_rdd.map(lambda elem: rnafold_obj.RNAfold_map_rule(elem, 4))
   
-  # Validating pri-mirna with mircheck
-  pri_vld_rdd = pri_fold_rdd.map(lambda elem: mircheck_obj.mirCheck_map_rule(elem, 4)).filter(lambda elem: any(elem[1][4]))
+    # Validating pri-mirna with mircheck  
+    pri_vld_rdd = pri_fold_rdd.map(lambda elem: mircheck_obj.mirCheck_map_rule(elem, 4)).filter(lambda elem: any(elem[1][4]))
 
-  # Filtering structure with branched loop  ####################################################
-  one_loop_rdd = pri_vld_rdd.filter(lambda elem: ut.containsOnly1loop (  elem[1][4][2][ int(elem[1][4][4]) : int(elem[1][4][5])+1 ] ))
+    # Filtering structure with branched loop
+    one_loop_rdd = pri_vld_rdd.filter(lambda elem: ut.containsOnly1loop (  elem[1][4][2][ int(elem[1][4][4]) : int(elem[1][4][5])+1 ] ))
 
-  # Extraction of the pre-miRNA
-  premir_rdd = one_loop_rdd.map(prec_obj.extract_prem_rule)
+    # Extraction of the pre-miRNA
+    premir_rdd = one_loop_rdd.map(prec_obj.extract_prem_rule)  
 
-  # pre-miRNA folding
-  pre_fold_rdd = premir_rdd.map(lambda elem: rnafold_obj.RNAfold_map_rule(elem, 5))
+    # pre-miRNA folding
+    pre_fold_rdd = premir_rdd.map(lambda elem: rnafold_obj.RNAfold_map_rule(elem, 5))
   
-  # Validating pre-mirna with mircheck
-  pre_vld_rdd = pre_fold_rdd.map(lambda elem: mircheck_obj.mirCheck_map_rule(elem, 5)).filter(lambda elem: any(elem[1][5]))
+    # Validating pre-mirna with mircheck
+    pre_vld_rdd = pre_fold_rdd.map(lambda elem: mircheck_obj.mirCheck_map_rule(elem, 5)).filter(lambda elem: any(elem[1][5]))
 
-  # you can use chromo_strand as key to search bowtie blocs in the following dict
-  dict_bowtie_chromo_strand = ut.return_Bowtie_strandchromo_dict (bowtie_rdd.collect())
+    # you can use chromo_strand as key to search bowtie blocs in the following dict
+    dict_bowtie_chromo_strand = ut.return_Bowtie_strandchromo_dict (bowtie_rdd.collect())
 
-  # results of miRNA prediction
-  miRNA_rdd = pre_vld_rdd.filter(lambda elem: profile_obj.functionX(elem, dict_bowtie_chromo_strand) )
-  results = miRNA_rdd.collect()
-  #print results
-  #print len(results)
+    # results of miRNA prediction
+    miRNA_rdd = pre_vld_rdd.filter(lambda elem: profile_obj.functionX(elem, dict_bowtie_chromo_strand) )
+    results = miRNA_rdd.collect()
+
+    # write results to a file
+    outfile = rep_output + inBasename + '_miRNAprediction.txt'
+    ut.writeToFile (results, outfile)
+   
+
   sc.stop() #allow to run multiple SparkContexts
-
-  outfile = rep_data + inBasename + '_miRNAprediction.txt'
-  ut.writeToFile (results, outfile)
-
-
-if __name__ == '__main__' :
-
-  if not len(sys.argv) == 3:
-    sys.stderr.write('Two arguments required\nUsage: spark-submit mirLibPipeline.py <path to paramfile> <path to your input> 2>/dev/null\n')
-    sys.exit()
-
-  paramfile = sys.argv[1]
-  paramDict = ut.readparam (paramfile)
-
-  # batch option
-  batch = paramDict['batch']
-  if not batch == 'true':
-    print 'launching one file'
-    infile = sys.argv[2]
-    SparkTask(paramDict, infile)
-  else:
-    print 'launching batch files'
-    mypath = sys.argv[2]
-    infiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    for infile in infiles:
-      SparkTask(paramDict, mypath+infile)
-    
 
 
 
