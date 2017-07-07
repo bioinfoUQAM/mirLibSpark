1b18d214a12dbc88e6ddf8b8e647aef055a398cc
Julie Wu <wu.chaojung@gmail.com>
2017-07-07 00:00:32 -0400

    modify path related to HOME

--- src/mirTest.py
+++ src/mirTest.py
@@ -70,12 +70,14 @@ if __name__ == '__main__' :
   mirdup_tmp_file = rep_tmp + 'sequencesToValidate_bymirdup.txt'
   mirdup_model = project_path + '/lib/miRdup_1.4/model/' + paramDict['mirdup_model']
   # miRanda parameter
+  mirdup_jar = project_path + '/lib/miRdup_1.4/miRdup.jar'
   Max_Score_cutoff = float(paramDict['Max_Score_cutoff'])
   query_motif_match_cutoff = float(paramDict['query_motif_match_cutoff'])
   gene_motif_match_cutoff = float(paramDict['gene_motif_match_cutoff'])
   Max_Energy_cutoff = float(paramDict['Max_Energy_cutoff'])
   target_file = project_path + '/lib/' + paramDict['target_file']
   miranda_tmp_file = rep_tmp + 'tmp_mirna_seq.txt'
+  miranda_exe = project_path + '/lib/miranda'
   
   reps = [rep_output, rep_tmp, rep_msub_jobsOut]
   ut.makedirs_reps (reps)
@@ -111,8 +113,8 @@ if __name__ == '__main__' :
   rnafold_obj = mru.prog_RNAfold()
   mircheck_obj = mru.prog_mirCheck(mcheck_param)
   profile_obj = mru.prog_dominant_profile()
-  mirdup_obj = mru.prog_miRdup (mirdup_tmp_file, mirdup_model)
-  miranda_obj = mru.prog_miRanda(Max_Score_cutoff, query_motif_match_cutoff, gene_motif_match_cutoff, Max_Energy_cutoff, target_file, miranda_tmp_file)
+  mirdup_obj = mru.prog_miRdup (mirdup_tmp_file, mirdup_model, mirdup_jar)
+  miranda_obj = mru.prog_miRanda(Max_Score_cutoff, query_motif_match_cutoff, gene_motif_match_cutoff, Max_Energy_cutoff, target_file, miranda_tmp_file, miranda_exe)
 
   # Fetch library files in mypath
   infiles = [f for f in listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
@@ -236,3 +238,5 @@ if __name__ == '__main__' :
   outTime = rep_output + appId + '_time.txt'
   ut.writeTimeLibToFile (timeDict, outTime, appId, paramDict)
   #'''
+
+
