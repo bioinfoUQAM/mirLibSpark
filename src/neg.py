fd7234feb1eb58d2ad4ecd18ab990dce043b15b2
Julie <wu.chaojung@gmail.com>
2017-08-18 12:37:27 -0700

    create neg set from all genome both strands

--- src/neg.py
+++ src/neg.py
@@ -7,8 +7,6 @@ date: 2017-08-17
 import utils as ut
 import random
 
-
-
 def collect_negtive_miRNA ():
   known_ath100 = []
   #f_known = '/home/cloudera/Desktop/mirLibHadoop/test/high_conf_mature_ath_uniq.txt'
@@ -20,16 +18,18 @@ def collect_negtive_miRNA ():
   genome_path = '/home/cloudera/workspace/mirLibHadoop/dbs/ATH/Genome/'
   file_ext = '.fas'
   genome = ut.getGenome (genome_path, file_ext)
-  chr1seq = genome['Chr1']
-  lenchr1 = len(chr1seq)
+  genomeseq_all = ''
+  for k, v in genome.items():
+    genomeseq_all += v + ut.getRevComp (v)
+  lengenome = len(genomeseq_all)
   negseqs = []
 
   i = 1
   while i < 1001:
-    start = random.randint(0, lenchr1-25)
+    start = random.randint(0, lengenome-25)
     end = random.randint(start+17, start+24)
     length = end - start + 1
-    seq = chr1seq[start: end+1]
+    seq = genomeseq_all[start: end+1]
     if seq[:10] in known_ath100: continue
     if seq[-10:] in known_ath100: continue
     if seq not in negseqs: 
@@ -41,7 +41,7 @@ negseqs = collect_negtive_miRNA ()
 outfile = '../negativeSet/neg_ath1000.txt'
 fh_out = open (outfile, 'w')
 for s in negseqs:
-  print >> fh_out, s, '\t', 500
+  print >> fh_out, s, '\t500'
 fh_out.close()
 
 
