e49f14a9cb3c189491f60aa9d77a64f031e013c8
Julie <wu.chaojung@gmail.com>
2018-02-12 14:43:39 -0800

    wip miranda wrapper

---
+++ src/run_miranda.py
@@ -0,0 +1,15 @@
+import mirLibRules as mru
+
+project_path='/home/cloudera/workspace/mirLibHadoop/'
+miranda_exe = project_path + '/lib/miranda'
+
+Max_Score_cutoff=170
+Max_Energy_cutoff=-15
+Gap_Penalty=-15
+
+target_file = project_path + '/dbs/' + 'TAIR10_cdna_20101214_updated.fasta'
+rep_tmp = project_path + '/tmp/' 
+
+miranda_obj = mru.prog_miRanda(Max_Score_cutoff, Max_Energy_cutoff, target_file, rep_tmp, miranda_exe, Gap_Penalty)
+
+print('test15')
