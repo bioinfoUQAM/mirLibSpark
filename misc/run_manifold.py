1ce4db3b7923ade9b1d56275078bb0c37701cf71
Julie Wu <wu.chaojung@gmail.com>
2018-02-23 17:31:16 -0500

    manifold

---
+++ misc/run_manifold.py
@@ -0,0 +1,105 @@
+print(__doc__)
+
+from time import time
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.ticker import NullFormatter
+
+from sklearn import manifold, datasets
+
+# Next line to silence pyflakes. This import is needed.
+Axes3D
+
+
+
+		
+
+infile = 'testset.csv' #= csv format
+import pandas as pd
+X = pd.read_csv(infile, header= None)
+
+n_points = 127
+#X, color = datasets.samples_generator.make_s_curve(n_points, random_state=0)
+n_neighbors, n_components = 20, 4
+
+
+
+#'''
+fig = plt.figure(figsize=(15, 8))
+plt.suptitle("Manifold Learning with %i points, %i neighbors, %i components"
+             % (n_points, n_neighbors, n_components), fontsize=14)
+
+
+methods = ['standard', 'ltsa', 'hessian', 'modified']
+labels = ['LLE', 'LTSA', 'Hessian LLE', 'Modified LLE']
+
+for i, method in enumerate(methods):
+    t0 = time()
+    Y = manifold.LocallyLinearEmbedding(n_neighbors, n_components,
+                                        eigen_solver='auto',
+                                        method=method).fit_transform(X)
+    t1 = time()
+    print("%s: %.2g sec" % (methods[i], t1 - t0))
+
+    ax = fig.add_subplot(252 + i)
+    plt.scatter(Y[:, 0], Y[:, 1], cmap=plt.cm.Spectral)#plt.scatter(Y[:, 0], Y[:, 1], c=plt.cm.Set1(Y[i] *10.), cmap=plt.cm.Spectral)
+    plt.title("%s (%.2g sec)" % (labels[i], t1 - t0))
+    ax.xaxis.set_major_formatter(NullFormatter())
+    ax.yaxis.set_major_formatter(NullFormatter())
+    plt.axis('tight')
+
+t0 = time()
+Y = manifold.Isomap(n_neighbors, n_components).fit_transform(X)
+t1 = time()
+print("Isomap: %.2g sec" % (t1 - t0))
+ax = fig.add_subplot(257)
+plt.scatter(Y[:, 0], Y[:, 1], cmap=plt.cm.Spectral)
+plt.title("Isomap (%.2g sec)" % (t1 - t0))
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+
+t0 = time()
+mds = manifold.MDS(n_components, max_iter=100, n_init=1)
+Y = mds.fit_transform(X)
+t1 = time()
+print("MDS: %.2g sec" % (t1 - t0))
+ax = fig.add_subplot(258)
+plt.scatter(Y[:, 0], Y[:, 1], cmap=plt.cm.Spectral)
+plt.title("MDS (%.2g sec)" % (t1 - t0))
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+
+t0 = time()
+se = manifold.SpectralEmbedding(n_components=n_components,
+                                n_neighbors=n_neighbors)
+Y = se.fit_transform(X)
+t1 = time()
+print("SpectralEmbedding: %.2g sec" % (t1 - t0))
+ax = fig.add_subplot(259)
+plt.scatter(Y[:, 0], Y[:, 1], cmap=plt.cm.Spectral)
+plt.title("SpectralEmbedding (%.2g sec)" % (t1 - t0))
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+t0 = time()
+tsne = manifold.TSNE(n_components=n_components, init='pca', random_state=0)
+Y = tsne.fit_transform(X)
+t1 = time()
+print("t-SNE: %.2g sec" % (t1 - t0))
+ax = fig.add_subplot(2, 5, 10)
+plt.scatter(Y[:, 0], Y[:, 1], cmap=plt.cm.Spectral)
+#plot_embedding(tsne.fit_transform(digits.data))
+
+plt.title("t-SNE (%.2g sec)" % (t1 - t0))
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+plt.show()
+#'''
