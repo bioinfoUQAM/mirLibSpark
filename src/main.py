be5791c8b447b6e0a886c548f609014e9c2ca91b
Julie Wu <wu.chaojung@gmail.com>
2018-09-20 15:19:55 -0400

    main

--- src/main.py
+++ src/main.py
@@ -3,15 +3,15 @@ import os
 #cmd = 'python purge.py'
 #os.system (cmd)
 
-cmd = 'git pull origin smooth'
+cmd = 'git pull origin bowtiesplit'
 os.system (cmd)
 
 #cmd = 'rm -fr ../input/fake_a3.txt'
 #os.system(cmd)
 
 
-cmd = 'cp ../input_samples/fake_a3.txt ../input'
-os.system(cmd)
+#cmd = 'cp ../input_samples/fake_a3.txt ../input'
+#os.system(cmd)
 
 
 cmd = 'time spark-submit mirLibPipeline.py ../paramfile.txt 2>/dev/null'
