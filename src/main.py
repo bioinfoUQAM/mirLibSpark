929e40951aa375e5f5995c4115c85f2dcf9e34b2
Julie Wu <wu.chaojung@gmail.com>
2018-09-20 18:09:07 -0400

    loop code

--- src/main.py
+++ src/main.py
@@ -14,17 +14,18 @@ os.system (cmd)
 #os.system(cmd)
 
 
-#cmd = 'time spark-submit mirLibPipeline.py ../paramfile.txt 2>/dev/null'
-#os.system(cmd)
-
-
-cmd = 'mkdir ../dbs/ATH/bowtie_split_index'
+cmd = 'time spark-submit mirLibPipeline.py ../paramfile.txt 2>/dev/null'
 os.system(cmd)
 
 
-chro = [1, 2, 3, 4, 5, 'C', 'M']
-for c in chro:
-  refgenome = '../dbs/ATH/Genome/TAIR10_chr' + str(c) + '.fas'
-  cmd = '../lib/bowtie-build -f ' + refgenome + ' atht10_' + 'chr' + str(c)
-  os.system(cmd)
+#cmd = 'mkdir ../dbs/ATH/bowtie_split_index'
+#os.system(cmd)
+
+
+#chro = [1, 2, 3, 4, 5, 'C', 'M']
+#for c in chro:
+#  refgenome = '../dbs/ATH/Genome/TAIR10_chr' + str(c) + '.fas'
+#  cmd = '../lib/bowtie-build -f ' + refgenome + ' atht10_' + 'chr' + str(c)
+#  os.system(cmd)
+
 
--- src/mirLibPipeline.py
+++ src/mirLibPipeline.py
@@ -242,10 +242,10 @@ if __name__ == '__main__' :
     ##  bowtie chromosome loop
     ##
     ##############################################################
-    chromosome = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    chromosome = 'chr1, chr2, chr3, chr4, chr5, chrC, chrM'.split(', ')
     mergebowtie = []
     for ch in chromosome:
-      bowtie_obj = mru.prog_bowtie(b_index_path)
+      bowtie_obj = mru.prog_bowtie(b_index_path + ch + '/')
       bowtie_cmd, bowtie_env = bowtie_obj.Bowtie_pipe_cmd()
 
 
