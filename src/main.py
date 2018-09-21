4c87f3de7a3f50c48de0e64767710b5937584a7d
Julie Wu <wu.chaojung@gmail.com>
2018-09-20 20:13:42 -0400

    main

--- src/main.py
+++ src/main.py
@@ -3,15 +3,15 @@ import os
 #cmd = 'python purge.py'
 #os.system (cmd)
 
-cmd = 'git pull origin bowtiesplit'
-os.system (cmd)
+#cmd = 'git pull origin bowtiesplit'
+#os.system (cmd)
 
 #cmd = 'rm -fr ../input/fake_a3.txt'
 #os.system(cmd)
 
 
-cmd = 'cp ../input_samples/fake_a3.txt ../input'
-os.system(cmd)
+#cmd = 'cp ../input_samples/fake_a3.txt ../input'
+#os.system(cmd)
 
 
 
