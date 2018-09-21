b83f44b08a278070978b31c423ada8d042f49a3f
Julie Wu <wu.chaojung@gmail.com>
2018-09-21 04:27:05 -0400

    final test before merge

--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
 import os
 
-cmd = 'python purge.py'
-os.system (cmd)
+#cmd = 'python purge.py'
+#os.system (cmd)
 
 cmd = 'git pull origin genomeextract'
 os.system (cmd)
 
-cmd = 'rm -fr ../input/fake_a3.txt'
-os.system(cmd)
 
 
 #cmd = 'cp ../input_samples/fake_a3.txt ../input'
+#cmd = 'rm -fr ../input/fake_a3.txt'
 #os.system(cmd)
 
 
-cmd = 'cp ../input_samples/100.txt ../input'
+#cmd = 'cp ../input_samples/100.txt ../input'
+cmd = 'rm ../input/100.txt'
 os.system(cmd)
 
 
