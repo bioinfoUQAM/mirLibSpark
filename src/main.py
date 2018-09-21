c16d316bedf938e851cd4777aff294357f43f4ad
Julie Wu <wu.chaojung@gmail.com>
2018-09-21 04:22:06 -0400

    test 100.txt

--- src/main.py
+++ src/main.py
@@ -1,21 +1,28 @@
 import os
 
-#cmd = 'python purge.py'
-#os.system (cmd)
+cmd = 'python purge.py'
+os.system (cmd)
 
 cmd = 'git pull origin genomeextract'
 os.system (cmd)
 
-#cmd = 'rm -fr ../input/fake_a3.txt'
-#os.system(cmd)
+cmd = 'rm -fr ../input/fake_a3.txt'
+os.system(cmd)
 
 
 #cmd = 'cp ../input_samples/fake_a3.txt ../input'
 #os.system(cmd)
 
 
+cmd = 'cp ../input_samples/100.txt ../input'
+os.system(cmd)
 
 
+
+
+
+cmd = 'time spark-submit mirLibPipeline.py ../paramfile.txt 2>/dev/null'
+os.system(cmd)
 
 
 #cmd = 'mkdir ../dbs/ATH/bowtie_split_index'
@@ -69,8 +76,4 @@ os.system (cmd)
 
 
 
-
-
-cmd = 'time spark-submit mirLibPipeline.py ../paramfile.txt 2>/dev/null'
-os.system(cmd)
 
