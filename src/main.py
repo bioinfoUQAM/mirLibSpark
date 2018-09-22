2d2a0930bd39e1139dca5990bb4f950270d4d15b
Julie Wu <wu.chaojung@gmail.com>
2018-09-22 00:48:25 -0400

    want to see fake_a3 results after zipindex

--- src/main.py
+++ src/main.py
@@ -9,9 +9,9 @@ os.system (cmd)
 
 
 
-#cmd = 'cp ../input_samples/fake_a3.txt ../input'
+cmd = 'cp ../input_samples/fake_a3.txt ../input'
 #cmd = 'rm -fr ../input/fake_a3.txt'
-#os.system(cmd)
+os.system(cmd)
 
 #cmd = 'cp ../input_samples/100.txt ../input'
 #cmd = 'rm -f ../input/100.txt'
