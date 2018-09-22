cc844f928cc6e8e61d0a3c15405da5badfd1ab47
Julie Wu <wu.chaojung@gmail.com>
2018-09-22 01:56:17 -0400

    sudo clear caches

--- src/purge.py
+++ src/purge.py
@@ -7,15 +7,16 @@ cmd = 'rm -fr ../tmp/*'
 os.system(cmd)
 
 
-cmd = 'su';os.system(cmd)
-cmd = 'cloudera';os.system(cmd)
-cmd = 'free -m';os.system(cmd)
-cmd = 'sync';os.system(cmd)
-cmd = 'echo 1 > /proc/sys/vm/drop_caches';os.system(cmd)
-cmd = 'echo 2 > /proc/sys/vm/drop_caches';os.system(cmd)
-cmd = 'echo 3 > /proc/sys/vm/drop_caches';os.system(cmd)
-cmd = 'free -m';os.system(cmd)
-cmd = 'exit';os.system(cmd)
+#cmd = 'su';os.system(cmd)
+#cmd = 'cloudera';os.system(cmd)
+#cmd = 'free -m';os.system(cmd)
+#cmd = 'sync';os.system(cmd)
+#cmd = 'echo 1 > /proc/sys/vm/drop_caches';os.system(cmd)
+#cmd = 'echo 2 > /proc/sys/vm/drop_caches';os.system(cmd)
+#cmd = 'echo 3 > /proc/sys/vm/drop_caches';os.system(cmd)
+#cmd = 'free -m';os.system(cmd)
+#cmd = 'exit';os.system(cmd)
 
+cmd = 'sudo sysctl -w vm.drop_caches=3 vm.drop_caches=0';os.system(cmd)
 
 
