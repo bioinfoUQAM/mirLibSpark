f80b45c41b1bb33636785b56de81dc393ab5782f
Julie Wu <wu.chaojung@gmail.com>
2018-09-23 05:21:24 -0400

    test master_predicted_distinctMiRNAs, use fake_a3

--- src/mirLibPipeline.py
+++ src/mirLibPipeline.py
@@ -419,10 +419,11 @@ if __name__ == '__main__' :
   
   #= in: ( lib, ('seq', [freq, nbLoc, ['strd','chr',posChr], ['priSeq',posMirPri,'priFold', 'mkPred','mkStart','mkStop'], ['preSeq',posMirPre,'preFold','mpPred','mpScore'], totalfrq]) )
   libRESULTS_rdd = sc.parallelize(libRESULTS, partition).flatMap(lambda e: e[1]) ## update
-  print('libRESULTS_rdd', libRESULTS_rdd.collect())
+  #print('libRESULTS_rdd', libRESULTS_rdd.collect())
 
   #= out: miRNAseq
-  #master_predicted_distinctMiRNAs = libRESULTS_rdd.map(lambda e: e[1][0]).distinct().collect() ## update
+  master_predicted_distinctMiRNAs = libRESULTS_rdd.map(lambda e: e[0]).distinct().collect() ## update
+  print(master_predicted_distinctMiRNAs)
 
   #= out: [miRNAseq, strand, chromo, posChr, preSeq, posMirPre, preFold, mkPred, newfbstart, newfbstop, mpPred, mpScore]
   #master_distinctPrecursor_infos = libRESULTS_rdd.map( ut.xrule )
