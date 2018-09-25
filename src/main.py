4594714c1d58f4a693147d3dcff9f95c1690d066
Julie Wu <wu.chaojung@gmail.com>
2018-09-25 17:01:07 -0400

    init

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import os
 
 #cmd = 'python purge.py';os.system (cmd)
 
-cmd = 'git pull origin tgkegg';os.system (cmd)
+cmd = 'git pull origin tgkegg2nd';os.system (cmd)
 
 
 
@@ -20,7 +20,8 @@ cmd = 'rm -fr ../input/fake_a3.txt';os.system(cmd)
 cmd = 'git remote prune origin';os.system(cmd)
 #cmd = 'git fetch origin';os.system(cmd)
 #cmd = 'git checkout master';os.system(cmd)
-#cmd = 'git branch -D printout';os.system(cmd)
+cmd = 'git branch -D tgkegg';os.system(cmd)
+cmd = 'git branch';os.system(cmd)
 
 
 cmd = 'free -m';os.system(cmd)
