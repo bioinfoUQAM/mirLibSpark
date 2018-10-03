0bee27f2eaf76519217baf30c35204d1fe1cd8e8
Julie <wu.chaojung@gmail.com>
2018-10-03 13:59:34 -0700

    k

--- outputJEAN/plot_loci.py
+++ outputJEAN/plot_loci.py
@@ -17,28 +17,65 @@ def getRevComp (seq):
 
 
 genome_seq_file = '../dbs/JEAN/Genome/XLOC_126981_6D_REVCOMP.fasta'
-sRNAmappings_file = 'demo_loci.txt'
+sRNAmappings_file = 'demo_loci.txt' # bowtie result: seq	freq	nbloci	strand	chromo	poschr
+with open (genome_seq_file, 'r') as fh: 
+  genseq = [x.rstrip('\n').split('\t') for x in fh.readlines()][1][0]
+with open (sRNAmappings_file, 'r') as fh: 
+  mappings = [x.rstrip('\n').split('\t') for x in fh.readlines()]
 
-with open (genome_seq_file, 'r') as fh: genseq = [x.rstrip('\n').split('\t') for x in fh.readlines()][1][0]
+# newdata = []
+# for m in mappings: 
+  # srna = m[0]
+  # freq = m[1]
+  # strd = str(m[3])
+  # if strd == '-': srna = getRevComp (srna)
+  # if strd == '+': strd = 'p'
+  # else: strd = 'n'
+  # posch= m[5]
+  # line = str(posch).zfill(4) + '.'*int(posch) + srna + '_' + freq + '_' + strd + '_' + str(len(srna))
+  # newdata.append(line)
+# newdata = sorted(newdata)
+# for line in newdata:
+  # print(line)
+# print('....' + genseq)
 
+#print(len(genseq))
+d_pos_freq = {}
+for i in range (1, len (genseq) + 1 ):
+  d_pos_freq[i] = 0
 
-
-with open (sRNAmappings_file, 'r') as fh: mappings = [x.rstrip('\n').split('\t') for x in fh.readlines()]
-
-newdata = []
-for m in mappings: 
+for m in mappings:
   srna = m[0]
-  freq = m[1]
-  strd = str(m[3])
-  if strd == '-': srna = getRevComp (srna)
-  if strd == '+': strd = 'p'
-  else: strd = 'n'
-  posch= m[5]
-  line = str(posch).zfill(4) + '.'*int(posch) + srna + '_' + freq + '_' + strd + '_' + str(len(srna))
-  newdata.append(line)
+  freq = int(m[1])
+  strd = m[3]
+  posch= int(m[5])
+  if strd == '-': posch = posch - 2
+  if len(srna) == 21:
+    d_pos_freq[posch] += freq
+  
+#for k, v in d_pos_freq.items():
+#  print(k, v)
 
-newdata = sorted(newdata)
-for line in newdata:
-  print(line)
-print('....' + genseq)
+
+for i in range (2, len (genseq) + 1 - 21*7):
+  n = 0
+  k1 = d_pos_freq[i]
+  if k1 > 0: n += 1
+  k2 = d_pos_freq[i+21*1]
+  if k2 > 0: n += 1
+  k3 = d_pos_freq[i+21*2]
+  if k3 > 0: n += 1
+  k4 = d_pos_freq[i+21*3]
+  if k4 > 0: n += 1
+  k5 = d_pos_freq[i+21*4]
+  if k5 > 0: n += 1
+  k6 = d_pos_freq[i+21*5]
+  if k6 > 0: n += 1
+  k7 = d_pos_freq[i+21*6]
+  if k7 > 0: n += 1
+  k8 = d_pos_freq[i+21*7]
+  if k8 > 0: n += 1
+  sum1_8 = sum([k1, k2, k3, k4, k5, k6, k7, k8])
+  print(i, sum1_8, n)
+
 
