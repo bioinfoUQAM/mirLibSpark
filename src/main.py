f2e48b33f266c56c0d5fa161f3d1421b11dfca16
Julie Wu <wu.chaojung@gmail.com>
2018-10-03 03:16:05 -0400

    main

--- src/main.py
+++ src/main.py
@@ -12,10 +12,10 @@ import os
 #cmd = 'git checkout master';os.system(cmd)
 
 
-#cmd = 'cp ../input_samples/fake_a.txt ../input';os.system(cmd)
+cmd = 'cp ../input_samples/fake_a.txt ../input';os.system(cmd)
 #cmd = 'rm -fr ../input/fake_a.txt';os.system(cmd)
 
-#cmd = 'cp ../input_samples/fake_a3.txt ../input';os.system(cmd)
+cmd = 'cp ../input_samples/fake_a3.txt ../input';os.system(cmd)
 #cmd = 'rm -fr ../input/fake_a3.txt';os.system(cmd)
 
 #cmd = 'cp ../input_samples/wheat_a1_3AS.txt ../input';os.system(cmd)
