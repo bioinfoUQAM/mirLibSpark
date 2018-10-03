f71da3af592e37e1fcf275230681e944797ea792
Julie Wu <wu.chaojung@gmail.com>
2018-10-03 01:18:21 -0400

    pyspark submit file

--- workdir/cedar_training/pyspark_submit_j.sh
+++ workdir/cedar_training/pyspark_submit_j.sh
@@ -25,18 +25,26 @@ export SPARK_IDENT_STRING=$SLURM_JOBID
 export SPARK_WORKER_DIR=$SLURM_TMPDIR
 start-master.sh
 
-(
-export SPARK_NO_DAEMONIZE=1;
-srun -x $(hostname -s) -n $((SLURM_NTASKS -1)) --label --output=$SPARK_LOG_DIR/spark-$SPARK_IDENT_STRING-workers.out \
-             start-slave.sh -m ${SLURM_MEM_PER_NODE}M -c ${SLURM_CPUS_PER_TASK} spark://$(hostname -f):7077
-) &
+sleep 1
+MASTER_URL=$(grep -Po '(?=spark://).*' $SPARK_LOG_DIR/spark-${SPARK_IDENT_STRING}-org.apache.spark.deploy.master*.out)
 
-spark-submit --executor-memory ${SLURM_MEM_PER_NODE}M $SPARK_HOME/pi.py 100000
+NWORKERS=$((SLURM_NTASKS - 1))
+SPARK_NO_DAEMONIZE=1 srun -n ${NWORKERS} -N ${NWORKERS} --label --output=$SPARK_LOG_DIR/spark-%j-workers.out start-slave.sh -m ${SLURM_MEM_PER_NODE}M -c ${SLURM_CPUS_PER_TASK} ${MASTER_URL} &
+slaves_pid=$!
 
+
+srun -n 1 -N 1 spark-submit --master ${MASTER_URL} --executor-memory ${SLURM_MEM_PER_NODE}M $SPARK_HOME/pi.py 100000
+
+kill $slaves_pid
 stop-master.sh
 
 
+
+
+
+
 #= sbatch pyspark_submit.sh
+#= sbatch pyspark_submit_j.sh
 #= squeue -u cjwu
 
 #181002-2031
