4617edcfc9190056163ae846a466d0959e1b7cd2
Julie Wu <wu.chaojung@gmail.com>
2018-10-09 14:07:43 -0400

    modify pyspark_submit_j.sh

--- workdir/pyspark_submit_j.sh
+++ workdir/pyspark_submit_j.sh
@@ -13,12 +13,12 @@
 #= preloaded: python2.7, perl, java
 #= module loaded: pyspark, duskmasker, bowtie, RNAfold
 #= included dependencies: miranda, VARNA
-module load nixpkgs/16.09
+#module load nixpkgs/16.09
 module load spark/2.3.0
-module load gcc/5.4.0
-module load viennarna/2.4.9
-module load bowtie/1.1.2
-module load blast+/2.6.0
+#module load gcc/5.4.0
+#module load viennarna/2.4.9
+#module load bowtie/1.1.2
+#module load blast+/2.6.0
 
 
 export SPARK_IDENT_STRING=$SLURM_JOBID
@@ -29,11 +29,12 @@ sleep 1
 MASTER_URL=$(grep -Po '(?=spark://).*' $SPARK_LOG_DIR/spark-${SPARK_IDENT_STRING}-org.apache.spark.deploy.master*.out)
 
 NWORKERS=$((SLURM_NTASKS - 1))
-SPARK_NO_DAEMONIZE=1 srun -n ${NWORKERS} -N ${NWORKERS} --label --output=$SPARK_LOG_DIR/spark-%j-workers.out start-slave.sh -m ${SLURM_MEM_PER_NODE}M -c ${SLURM_CPUS_PER_TASK} ${MASTER_URL} &
+#SPARK_NO_DAEMONIZE=1 srun -n ${NWORKERS} -N ${NWORKERS} --label --output=$SPARK_LOG_DIR/spark-%j-workers.out start-slave.sh -m ${SLURM_MEM_PER_NODE}M -c ${SLURM_CPUS_PER_TASK} ${MASTER_URL} &
+SPARK_NO_DAEMONIZE=1 --label --output=$SPARK_LOG_DIR/spark-%j-workers.out start-slave.sh -m ${SLURM_MEM_PER_NODE}M -c ${SLURM_CPUS_PER_TASK} ${MASTER_URL} &
 slaves_pid=$!
 
 
-srun -n 1 -N 1 spark-submit --master ${MASTER_URL} --executor-memory ${SLURM_MEM_PER_NODE}M $SPARK_HOME/cjwu/project/cjwu/gitrepo/mirLibSpark/workdir/pi.py 100000
+srun -n 1 -N 1 spark-submit --master ${MASTER_URL} --executor-memory ${SLURM_MEM_PER_NODE}M $SPARK_HOME/cjwu/project/cjwu/gitRepo/mirLibSpark/workdir/pi.py 100000
 
 kill $slaves_pid
 stop-master.sh
