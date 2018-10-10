bae4fc93a9df5d1851095ba64150ad124a3d7ee9
Julie <wu.chaojung@gmail.com>
2018-10-10 12:47:04 -0700

    init_dbs_ensembl40.py good

--- src/init_dbs_ensembl40.py
+++ src/init_dbs_ensembl40.py
@@ -5,18 +5,20 @@ Chao-Jung Wu
 python main.py ath 1
 
 species: 
-ath: 	Arabidopsis_thaliana.TAIR10				python main.py ath 1|2
-wheat: 	Triticum_aestivum.IWGSC					python main.py wheat 2
-corn: 	Zea_mays.AGPv4						python main.py corn 2
-rice: 	Oryza_sativa.IRGSP-1.0					python main.py rice 1
-potato: Solanum_tuberosum.SolTub_3.0				python main.py potato 1
-brome: 	Brachypodium_distachyon.Brachypodium_distachyon_v3.0	python main.py brome 1
-wheatD: Aegilops_tauschii.ASM34733v1				python main.py wheatD 1
+ath: 	Arabidopsis_thaliana.TAIR10				python init_dbs_ensembl40.py ath 1|2	tested_previously_ok
+wheat: 	Triticum_aestivum.IWGSC					python init_dbs_ensembl40.py wheat 2	tested_previously_ok
+corn: 	Zea_mays.AGPv4						python init_dbs_ensembl40.py corn 2
+rice: 	Oryza_sativa.IRGSP-1.0					python init_dbs_ensembl40.py rice 1	tested_181010_ok
+potato: Solanum_tuberosum.SolTub_3.0				python init_dbs_ensembl40.py potato 1
+brome: 	Brachypodium_distachyon.Brachypodium_distachyon_v3.0	python init_dbs_ensembl40.py brome 1
+wheatD: Aegilops_tauschii.ASM34733v1				python init_dbs_ensembl40.py wheatD 1
 '''
 
 
 import sys, os
 import gzip, shutil
+
+ensembl_version = 'release-40'
 
 def unzip_gzFile (gzFile):
   f_in = gzip.open(gzFile, 'rb')
@@ -75,12 +77,12 @@ if __name__ == '__main__' :
     if not os.path.exists(rep3): os.makedirs(rep3)
 
     #= get annotation files
-    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/arabidopsis_thaliana/cdna/Arabidopsis_thaliana.TAIR10.cdna.all.fa.gz'
+    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/arabidopsis_thaliana/cdna/Arabidopsis_thaliana.TAIR10.cdna.all.fa.gz'
     wanted_file = 'Arabidopsis_thaliana.TAIR10.cdna.all.fa.gz'
     curl_and_unzip_file (URL, wanted_file)
     move_file_to (wanted_file, rep1)
 
-    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/arabidopsis_thaliana/ncrna/Arabidopsis_thaliana.TAIR10.ncrna.fa.gz'
+    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/arabidopsis_thaliana/ncrna/Arabidopsis_thaliana.TAIR10.ncrna.fa.gz'
     wanted_file = 'Arabidopsis_thaliana.TAIR10.ncrna.fa.gz'
     curl_and_unzip_file (URL, wanted_file)
     move_file_to (wanted_file, rep1)
@@ -90,7 +92,7 @@ if __name__ == '__main__' :
       if not os.path.exists(repAll): os.makedirs(repAll)
 
       wanted_file = 'Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz'
-      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/arabidopsis_thaliana/dna/' + wanted_file
+      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/arabidopsis_thaliana/dna/' + wanted_file
       curl_and_unzip_file (URL, wanted_file)
     
       bowtieBuild (wanted_file[:-3], key)
@@ -99,7 +101,7 @@ if __name__ == '__main__' :
 
       IDs = '1 2 3 4 5 Mt Pt'.split(' ')
       for ID in IDs:
-        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.chromosome.'+ ID +'.fa.gz'
+        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.chromosome.'+ ID +'.fa.gz'
         wanted_file = URL.split('/')[-1]
         curl_and_unzip_file (URL, wanted_file)
         move_chromosomeFile_to (wanted_file, ID, rep2)
@@ -110,7 +112,7 @@ if __name__ == '__main__' :
         rep_ch = rep3 + ID + '/'
         if not os.path.exists(rep_ch): os.makedirs(rep_ch)
 
-        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.chromosome.'+ ID +'.fa.gz'
+        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.chromosome.'+ ID +'.fa.gz'
         wanted_file = URL.split('/')[-1]
         curl_and_unzip_file (URL, wanted_file)
 
@@ -132,7 +134,7 @@ if __name__ == '__main__' :
     if not os.path.exists(rep3): os.makedirs(rep3)
 
     #= get annotation files
-    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/triticum_aestivum/cdna/Triticum_aestivum.IWGSC.cdna.all.fa.gz'
+    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/triticum_aestivum/cdna/Triticum_aestivum.IWGSC.cdna.all.fa.gz'
     wanted_file = 'Triticum_aestivum.IWGSC.cdna.all.fa.gz'
     curl_and_unzip_file (URL, wanted_file)
     move_file_to (wanted_file, rep1)
@@ -147,7 +149,7 @@ if __name__ == '__main__' :
         rep_ch = rep3 + ID + '/'
         if not os.path.exists(rep_ch): os.makedirs(rep_ch)
 
-        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/triticum_aestivum/dna/Triticum_aestivum.IWGSC.dna.chromosome.'+ ID +'.fa.gz'
+        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/triticum_aestivum/dna/Triticum_aestivum.IWGSC.dna.chromosome.'+ ID +'.fa.gz'
         wanted_file = URL.split('/')[-1]
         curl_and_unzip_file (URL, wanted_file)
 
@@ -169,12 +171,12 @@ if __name__ == '__main__' :
     if not os.path.exists(rep3): os.makedirs(rep3)
 
     #= get annotation files
-    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/oryza_sativa/cdna/Oryza_sativa.IRGSP-1.0.cdna.all.fa.gz'
+    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/oryza_sativa/cdna/Oryza_sativa.IRGSP-1.0.cdna.all.fa.gz'
     wanted_file = URL.split('/')[-1]
     curl_and_unzip_file (URL, wanted_file)
     move_file_to (wanted_file, rep1)
 
-    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/oryza_sativa/ncrna/Oryza_sativa.IRGSP-1.0.ncrna.fa.gz'
+    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/oryza_sativa/ncrna/Oryza_sativa.IRGSP-1.0.ncrna.fa.gz'
     wanted_file = URL.split('/')[-1]
     curl_and_unzip_file (URL, wanted_file)
     move_file_to (wanted_file, rep1)
@@ -187,7 +189,7 @@ if __name__ == '__main__' :
       repAll = rep3 + 'All/'
       if not os.path.exists(repAll): os.makedirs(repAll)
 
-      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/oryza_sativa/dna/Oryza_sativa.IRGSP-1.0.dna.toplevel.fa.gz'
+      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/oryza_sativa/dna/Oryza_sativa.IRGSP-1.0.dna.toplevel.fa.gz'
       wanted_file = URL.split('/')[-1]
       curl_and_unzip_file (URL, wanted_file)
     
@@ -197,7 +199,7 @@ if __name__ == '__main__' :
 
       IDs = '1 2 3 4 5 6 7 8 9 10 11 12 Mt Pt'.split(' ')
       for ID in IDs:
-        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/oryza_sativa/dna/Oryza_sativa.IRGSP-1.0.dna.chromosome.'+ ID +'.fa.gz'
+        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/oryza_sativa/dna/Oryza_sativa.IRGSP-1.0.dna.chromosome.'+ ID +'.fa.gz'
         wanted_file = URL.split('/')[-1]
         curl_and_unzip_file (URL, wanted_file)
         move_chromosomeFile_to (wanted_file, ID, rep2)
@@ -215,12 +217,12 @@ if __name__ == '__main__' :
     if not os.path.exists(rep3): os.makedirs(rep3)
 
     #= get annotation files
-    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/solanum_tuberosum/cdna/Solanum_tuberosum.SolTub_3.0.cdna.all.fa.gz'
+    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/solanum_tuberosum/cdna/Solanum_tuberosum.SolTub_3.0.cdna.all.fa.gz'
     wanted_file = URL.split('/')[-1]
     curl_and_unzip_file (URL, wanted_file)
     move_file_to (wanted_file, rep1)
 
-    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/solanum_tuberosum/ncrna/Solanum_tuberosum.SolTub_3.0.ncrna.fa.gz'
+    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/solanum_tuberosum/ncrna/Solanum_tuberosum.SolTub_3.0.ncrna.fa.gz'
     wanted_file = URL.split('/')[-1]
     curl_and_unzip_file (URL, wanted_file)
     move_file_to (wanted_file, rep1)
@@ -233,7 +235,7 @@ if __name__ == '__main__' :
       repAll = rep3 + 'All/'
       if not os.path.exists(repAll): os.makedirs(repAll)
 
-      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/solanum_tuberosum/dna/Solanum_tuberosum.SolTub_3.0.dna.toplevel.fa.gz'
+      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/solanum_tuberosum/dna/Solanum_tuberosum.SolTub_3.0.dna.toplevel.fa.gz'
       wanted_file = URL.split('/')[-1]
       curl_and_unzip_file (URL, wanted_file)
     
@@ -243,12 +245,12 @@ if __name__ == '__main__' :
 
       IDs = '1 2 3 4 5 6 7 8 9 10 11 12'.split(' ')
       for ID in IDs:
-        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/solanum_tuberosum/dna/Solanum_tuberosum.SolTub_3.0.dna.chromosome.'+ ID +'.fa.gz'
+        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/solanum_tuberosum/dna/Solanum_tuberosum.SolTub_3.0.dna.chromosome.'+ ID +'.fa.gz'
         wanted_file = URL.split('/')[-1]
         curl_and_unzip_file (URL, wanted_file)
         move_chromosomeFile_to (wanted_file, ID, rep2)
       #= irregular naming of the chromosome
-      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/solanum_tuberosum/dna/Solanum_tuberosum.SolTub_3.0.dna.nonchromosomal.fa.gz'
+      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/solanum_tuberosum/dna/Solanum_tuberosum.SolTub_3.0.dna.nonchromosomal.fa.gz'
       wanted_file = URL.split('/')[-1]
       curl_and_unzip_file (URL, wanted_file)
       move_chromosomeFile_to (wanted_file, ID, rep2)
@@ -267,7 +269,7 @@ if __name__ == '__main__' :
     if not os.path.exists(rep3): os.makedirs(rep3)
 
     #= get annotation files
-    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/brachypodium_distachyon/cdna/Brachypodium_distachyon.Brachypodium_distachyon_v3.0.cdna.all.fa.gz'
+    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/brachypodium_distachyon/cdna/Brachypodium_distachyon.Brachypodium_distachyon_v3.0.cdna.all.fa.gz'
     wanted_file = URL.split('/')[-1]
     curl_and_unzip_file (URL, wanted_file)
     move_file_to (wanted_file, rep1)
@@ -280,7 +282,7 @@ if __name__ == '__main__' :
       repAll = rep3 + 'All/'
       if not os.path.exists(repAll): os.makedirs(repAll)
 
-      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/brachypodium_distachyon/dna/Brachypodium_distachyon.Brachypodium_distachyon_v3.0.dna.toplevel.fa.gz'
+      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/brachypodium_distachyon/dna/Brachypodium_distachyon.Brachypodium_distachyon_v3.0.dna.toplevel.fa.gz'
       wanted_file = URL.split('/')[-1]
       curl_and_unzip_file (URL, wanted_file)
     
@@ -290,12 +292,12 @@ if __name__ == '__main__' :
 
       IDs = '1 2 3 4 5'.split(' ')
       for ID in IDs:
-        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/solanum_tuberosum/dna/Solanum_tuberosum.SolTub_3.0.dna.chromosome.'+ ID +'.fa.gz'
+        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/solanum_tuberosum/dna/Solanum_tuberosum.SolTub_3.0.dna.chromosome.'+ ID +'.fa.gz'
         wanted_file = URL.split('/')[-1]
         curl_and_unzip_file (URL, wanted_file)
         move_chromosomeFile_to (wanted_file, ID, rep2)
       #= irregular naming of the chromosome
-      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/brachypodium_distachyon/dna/Brachypodium_distachyon.Brachypodium_distachyon_v3.0.dna.nonchromosomal.fa.gz'
+      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/brachypodium_distachyon/dna/Brachypodium_distachyon.Brachypodium_distachyon_v3.0.dna.nonchromosomal.fa.gz'
       wanted_file = URL.split('/')[-1]
       curl_and_unzip_file (URL, wanted_file)
       move_chromosomeFile_to (wanted_file, ID, rep2)
@@ -313,12 +315,12 @@ if __name__ == '__main__' :
     if not os.path.exists(rep3): os.makedirs(rep3)
 
     #= get annotation files
-    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/zea_mays/cdna/Zea_mays.AGPv4.cdna.all.fa.gz'
+    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/zea_mays/cdna/Zea_mays.AGPv4.cdna.all.fa.gz'
     wanted_file = URL.split('/')[-1]
     curl_and_unzip_file (URL, wanted_file)
     move_file_to (wanted_file, rep1)
 
-    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/zea_mays/ncrna/Zea_mays.AGPv4.ncrna.fa.gz'
+    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/zea_mays/ncrna/Zea_mays.AGPv4.ncrna.fa.gz'
     wanted_file = URL.split('/')[-1]
     curl_and_unzip_file (URL, wanted_file)
     move_file_to (wanted_file, rep1)
@@ -332,7 +334,7 @@ if __name__ == '__main__' :
       for ID in IDs:
         rep_ch = rep3 + ID + '/'
         if not os.path.exists(rep_ch): os.makedirs(rep_ch)
-        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/zea_mays/dna/Zea_mays.AGPv4.dna.chromosome.'+ ID +'.fa.gz'
+        URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/zea_mays/dna/Zea_mays.AGPv4.dna.chromosome.'+ ID +'.fa.gz'
         wanted_file = URL.split('/')[-1]
         curl_and_unzip_file (URL, wanted_file)
 
@@ -341,7 +343,7 @@ if __name__ == '__main__' :
         os.system(cmd)
         move_chromosomeFile_to (wanted_file, ID, rep2)
       #= irregular naming of the chromosome
-      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/zea_mays/dna/Zea_mays.AGPv4.dna.nonchromosomal.fa.gz'
+      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/zea_mays/dna/Zea_mays.AGPv4.dna.nonchromosomal.fa.gz'
       wanted_file = URL.split('/')[-1]
       curl_and_unzip_file (URL, wanted_file)
       move_chromosomeFile_to (wanted_file, ID, rep2)
@@ -359,12 +361,12 @@ if __name__ == '__main__' :
     if not os.path.exists(rep3): os.makedirs(rep3)
 
     #= get annotation files
-    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/aegilops_tauschii/cdna/Aegilops_tauschii.ASM34733v1.cdna.all.fa.gz'
+    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/aegilops_tauschii/cdna/Aegilops_tauschii.ASM34733v1.cdna.all.fa.gz'
     wanted_file = URL.split('/')[-1]
     curl_and_unzip_file (URL, wanted_file)
     move_file_to (wanted_file, rep1)
 
-    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/aegilops_tauschii/ncrna/Aegilops_tauschii.ASM34733v1.ncrna.fa.gz'
+    URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/aegilops_tauschii/ncrna/Aegilops_tauschii.ASM34733v1.ncrna.fa.gz'
     wanted_file = URL.split('/')[-1]
     curl_and_unzip_file (URL, wanted_file)
     move_file_to (wanted_file, rep1)
@@ -377,7 +379,7 @@ if __name__ == '__main__' :
       repAll = rep3 + 'All/'
       if not os.path.exists(repAll): os.makedirs(repAll)
 
-      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/aegilops_tauschii/dna/Aegilops_tauschii.ASM34733v1.dna.toplevel.fa.gz'
+      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/aegilops_tauschii/dna/Aegilops_tauschii.ASM34733v1.dna.toplevel.fa.gz'
       wanted_file = URL.split('/')[-1]
       curl_and_unzip_file (URL, wanted_file)
     
@@ -386,12 +388,12 @@ if __name__ == '__main__' :
       os.system(cmd)
 
       #= irregular naming of the chromosome
-      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/aegilops_tauschii/dna/Aegilops_tauschii.ASM34733v1.dna.toplevel.fa.gz'
+      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/aegilops_tauschii/dna/Aegilops_tauschii.ASM34733v1.dna.toplevel.fa.gz'
       wanted_file = URL.split('/')[-1]
       curl_and_unzip_file (URL, wanted_file)
       move_chromosomeFile_to (wanted_file, ID, rep2)
       #
-      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-40/fasta/aegilops_tauschii/dna/Aegilops_tauschii.ASM34733v1.dna.nonchromosomal.fa.gz'
+      URL = 'ftp://ftp.ensemblgenomes.org/pub/plants/' + ensembl_version + '/fasta/aegilops_tauschii/dna/Aegilops_tauschii.ASM34733v1.dna.nonchromosomal.fa.gz'
       wanted_file = URL.split('/')[-1]
       curl_and_unzip_file (URL, wanted_file)
       move_chromosomeFile_to (wanted_file, ID, rep2)
