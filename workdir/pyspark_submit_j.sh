a823c7438bd5b87b8efd8f28e35696b422c00c54
Julie Wu <wu.chaojung@gmail.com>
2018-10-11 20:52:27 -0400

    pyspark

--- workdir/pyspark_submit_j.sh
+++ workdir/pyspark_submit_j.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=5000M
 #SBATCH --cpus-per-task=6
 #SBATCH --ntasks-per-node=1
-#SBATCH --job-name=spk-1n6p2hr5G-mirLib-181010
+#SBATCH --job-name=spk-1n6p2hr5Gx-mirLib-181010
 #SBATCH --error=jobout/%x-%j.err
 #SBATCH --output=jobout/%x-%j.out
 #SBATCH --mail-user=wu.chaojung@gmail.com
@@ -35,8 +35,8 @@ slaves_pid=$!
 
 #= example:
 #= spark-submit --master ${MASTER_URL} --executor-memory ${SLURM_MEM_PER_NODE}M /home/cjwu/project/cjwu/gitRepo/mirLibSpark/workdir/cedar_training/pi.py 1000
-srun -n ${NWORKERS} -N ${NWORKERS} spark-submit --master ${MASTER_URL} --executor-memory ${SLURM_MEM_PER_NODE}M ../src/mirLibPipeline.py ../paramfile_WHEAT_IWGSC_graham.txt
-#srun -n ${NWORKERS} -N ${NWORKERS} spark-submit --master ${MASTER_URL} --executor-memory ${SLURM_MEM_PER_NODE}M --driver-memory 1200M ../src/mirLibPipeline.py ../paramfile_WHEAT_IWGSC_graham.txt
+srun -n ${NWORKERS} -N ${NWORKERS} spark-submit --master ${MASTER_URL} --executor-memory ${SLURM_MEM_PER_NODE}M /home/cjwu/project/cjwu/gitRepo/mirLibSpark/workdir/cedar_training/pi.py 1000
+#srun -n ${NWORKERS} -N ${NWORKERS} spark-submit --master ${MASTER_URL} --executor-memory ${SLURM_MEM_PER_NODE}M ../src/mirLibPipeline.py ../paramfile_WHEAT_IWGSC_graham.txt
 
 kill $slaves_pid
 stop-master.sh
