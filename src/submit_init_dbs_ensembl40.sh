fbcc9f9199d577f339b1cfc0cdce3d45e3cdbeb2
Julie Wu <wu.chaojung@gmail.com>
2018-10-20 12:13:12 -0400

    change name init_dbs_ensembl40_refactor.py init_dbs_ensembl40_v2.py

--- src/submit_init_dbs_ensembl40.sh
+++ src/submit_init_dbs_ensembl40.sh
@@ -14,12 +14,12 @@ module load nixpkgs/16.09
 module load gcc/5.4.0
 module load bowtie/1.1.2
 
-python init_dbs_ensembl40_refactor.py wheat 2 build	
-python init_dbs_ensembl40_refactor.py corn 2 build
-python init_dbs_ensembl40_refactor.py rice 1 build
-python init_dbs_ensembl40_refactor.py potato 1 build
-python init_dbs_ensembl40_refactor.py brome 1 build
-python init_dbs_ensembl40_refactor.py wheatD 1 build
+python init_dbs_ensembl40_v2.py wheat 2 build	
+python init_dbs_ensembl40_v2.py corn 2 build
+python init_dbs_ensembl40_v2.py rice 1 build
+python init_dbs_ensembl40_v2.py potato 1 build
+python init_dbs_ensembl40_v2.py brome 1 build
+python init_dbs_ensembl40_v2.py wheatD 1 build
 
 
 
