09ba84018deabc11a50d7b6239e15fc0bf41b6f7
JulieCJWu <wu.chaojung@gmail.com>
2018-10-23 08:15:48 -0700

    calculate_acc.py

---
+++ src/calculate_acc.py
@@ -0,0 +1,30 @@
+'''
+Author: Chao-Jung Wu
+Date: 2017-11-07
+'''
+
+def calculate_F1_score (tp, tn, fp, fn):
+  #= p: precision; r: recall
+  p = tp / float(tp + fp)
+  r = tp / float(tp + fn)
+  f1 = 2 * p * r / (p + r)
+  return round (f1, 3)
+
+def calculate_accuracy (tp, tn, fp, fn):
+  acc = (tp + tn)/ float(tp + tn + fp + fn)
+  return round (acc, 3)
+
+
+TP = 82
+TN = 990
+FP = 10
+FN = 18
+
+  
+f1 = calculate_F1_score (TP, TN, FP, FN)
+acc = calculate_accuracy (TP, TN, FP, FN)
+
+print(f1)
+print(acc)
+
+
