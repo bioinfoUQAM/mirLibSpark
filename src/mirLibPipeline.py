5c68239fe940391a3e4d9dfcdbc26e2aacfb173b
JulieCJWu <wu.chaojung@gmail.com>
2018-10-24 10:51:30 -0700

    I dont know why time increase, so I re-activate ln 371-373

--- src/mirLibPipeline.py
+++ src/mirLibPipeline.py
@@ -367,12 +367,13 @@ if __name__ == '__main__' :
     ## in : ('seq', [freq, nbLoc, ['strd','chr',posChr], ['priSeq',posMirPri,'priFold']])
     ## out: ('seq', [freq, nbLoc, ['strd','chr',posChr], ['priSeq',posMirPri,'priFold','mkPred','mkStart','mkStop']])
     pri_vld_rdd = pri_fold_rdd.map(lambda e: mircheck_obj.mirCheck_map_rule(e, 3))\
-                              .filter(lambda e: any(e[1][3]))
+                              .filter(lambda e: any(e[1][3]))\
+                              .map(lambda e: (e[0] + e[1][2][0] + e[1][2][1] + str(e[1][2][2]) + e[1][3][4] + e[1][3][5], e)  )\
+                              .reduceByKey(lambda a, b: a)\
+                              .map(lambda e: e[1])
     print('NB pri_vld_rdd (mircheck): ', pri_vld_rdd.count())
     print('current time:', datetime.datetime.now())
-    #.map(lambda e: (e[0] + e[1][2][0] + e[1][2][1] + str(e[1][2][2]) + e[1][3][4] + e[1][3][5], e)  )\
-    #.reduceByKey(lambda a, b: a)\
-    #.map(lambda e: e[1])
+
 
     #= Filtering len(pri-mirna) < 301 nt
     len300_rdd = pri_vld_rdd.filter(lambda e: (int(e[1][3][5]) - int(e[1][3][4])) < 301)
