88d647dbd6e39b34b8947059325087c4b27974c2
JulieCJWu <wu.chaojung@gmail.com>
2019-02-26 08:26:29 -0500

    load python/2.7.14 to override viennarna loading python3.7

--- workdir/mirlibspark_submission.sh
+++ workdir/mirlibspark_submission.sh
@@ -18,12 +18,14 @@
 #= preloaded: python2.7, perl, java
 #= module loaded: pyspark, duskmasker, bowtie, RNAfold
 #= included dependencies: miranda, VARNA
+#= viennarna will load python3.7, so need to reload python2.7.14 to override
 module load nixpkgs/16.09
 module load spark/2.3.0
 module load gcc/5.4.0
 module load viennarna/2.4.9
 module load bowtie/1.1.2
 module load blast+/2.6.0
+module load python/2.7.14
 #
 #= python requirements: statsmodels (includes: numpy, scipy, pandas, patsy), 
 #                       seaborn (includes: matplotlib)
