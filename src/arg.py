abefc79fd04fcd28d44cd0fe10316cb9cb2b9a4a
JulieCJWu <wu.chaojung@gmail.com>
2019-03-29 14:49:56 -0400

    debug int --> str

--- src/arg.py
+++ src/arg.py
@@ -73,7 +73,7 @@ def getOpt (parser):
     parser.add_argument('--sc_mstrmemory', default='20g')
     parser.add_argument('--sc_execmemory', default='20g')
     parser.add_argument('--sc_master', default='local[*]')
-    parser.add_argument('--sc_heartbeap', default=10)
+    parser.add_argument('--sc_heartbeap', default='10')
     #
     args = parser.parse_args()
     #
